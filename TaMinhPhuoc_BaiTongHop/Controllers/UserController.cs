9673d8ace1a1c9157292cdb8967b783e1a064249
phuoctm1910 <nothinginthis465@gmail.com>
2024-06-24 23:31:28 +0700

    Add project files.

---
+++ TaMinhPhuoc_BaiTongHop/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TaMinhPhuoc_BaiTongHop.Models;
+
+namespace TaMinhPhuoc_BaiTongHop.Controllers
+{
+    public class UserController : Controller
+    {
+        private static List<User> users = new List<User>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return Json(users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                users.Add(user);
+                return Json(new { success = true });
+            }
+            return BadRequest("Dữ liệu người dùng không hợp lệ.");
+        }
+    }
+}
---
+++ TaMinhPhuoc_BaiTongHop/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaMinhPhuoc_BaiTongHop.Models
+{
+    public class User
+    {
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string PhoneNumber {  get; set; }
+        [Required]
+        public string BirthDate { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
